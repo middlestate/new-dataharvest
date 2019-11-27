6b5c74cab320dcb4e2cc182e62880c1a599a1319
amitp <amitpatel_7@msn.com>
2019-11-27 13:01:38 -0800

    set background size to cover and position to center

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -2,7 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Hero = ({ background_image, title, subtitle, image }) => (
-  <div className="Hero" style={{background: `url(${background_image})`}}>
+  <div className="Hero" style={
+    {
+      background: `url(${background_image})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+    }
+  }>
     <h1 className="title">{title}</h1>
     <h3 className="subtitle">{subtitle}</h3>
     <img src={image} alt="hero" />
