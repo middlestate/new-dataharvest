6dee5b3f302f8e40d59f11583731e3958ce22ef7
amitp <amitpatel_7@msn.com>
2019-11-29 11:30:46 -0800

    created a hook for measuring viewport width

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-const Hero = ({ background_image, title, subtitle, image }) => (
-  <section className="Hero section" style={
-    {
-      background: `url(${background_image})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      height: '80vh'
-    }
-  }>
-    <img src={image} className="dataharvest-logo" alt="dataharvest_logo" />
-    <div className="title-container">
-      <h1 className="title">{title}</h1>
-      <h3 className="subtitle">{subtitle}</h3>    
-    </div>
-  </section>
-)
+const Hero = ({ background_image, title, subtitle, image }) => {
+  const useWindowWidth = () => {
+    // created a Hook to measure window width    
+    const [width, setWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth)
+      window.addEventListener('resize', handleResize)
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+    })
+
+    return width
+  }
+
+
+  
+  return (
+    <section className="Hero section" style={
+      {
+        background: `url(${background_image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        height: '80vh'
+      }
+    }>
+      <img src={image} className="dataharvest-logo" alt="dataharvest_logo" />
+      <div className="title-container">
+        <h1 className="title">{title}</h1>
+        <h3 className="subtitle">{subtitle}</h3>
+        <p className="subtitle">{useWindowWidth()}</p>
+      </div>
+    </section>
+  )
+}
 
 Hero.propTypes = {
   background_image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
