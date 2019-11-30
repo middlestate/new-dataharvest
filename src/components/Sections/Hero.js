d6cdfc70536944a1252ba6fb24dab1fb82b82d4e
amitp <amitpatel_7@msn.com>
2019-11-29 17:31:34 -0800

    converted Hero to class component - removed hook

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -1,40 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-const Hero = ({ background_image, title, subtitle, image }) => {
-  const useWindowWidth = () => {
-    const [width, setWidth] = useState(window.innerWidth)
-
-    useEffect(() => {
-      const handleResize = () => setWidth(window.innerWidth)
-      window.addEventListener('resize', handleResize)
-      return () => {
-        window.removeEventListener('resize', handleResize)
-      }
-    })
-    
-    return width
+class Hero extends React.Component {
+  constructor(props) {
+    super(props)
   }
 
-
-  
-  return (
-    <section className="Hero section" style={
-      {
-        background: `url(${background_image})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center center',
-        height: '80vh'
-      }
-    }>
-      <img src={image} className="dataharvest-logo" alt="dataharvest_logo" />
-      <div className="title-container">
-        <h1 className="title">{title}</h1>
-        <h3 className="subtitle">{subtitle}</h3>
-        {/* <p className="subtitle">{useWindowWidth()}</p> */}
-      </div>
-    </section>
-  )
+  render() {
+    return (
+      <section className="Hero section" style={
+        {
+          background: `url(${this.props.background_image})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+          height: '80vh'
+        }
+      }>
+        <img src={this.props.image} className="dataharvest-logo" alt="dataharvest_logo" />
+        <div className="title-container">
+          <h1 className="title">{this.props.title}</h1>
+          <h3 className="subtitle">{this.props.subtitle}</h3>
+          {/* <p className="subtitle">{useWindowWidth()}</p> */}
+        </div>
+      </section>
+    )
+  }
 }
 
 Hero.propTypes = {
