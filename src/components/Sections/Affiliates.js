fd2d76031753cf68285edadb66248785aaa3068e
amitp <amitpatel_7@msn.com>
2019-12-17 15:11:37 -0800

    added return statement

--- src/components/Sections/Affiliates.js
+++ src/components/Sections/Affiliates.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Affiliates = ({ logosArray }) => (
-  <div className="logo-row">
-    {logosArray.map(({ logo }, keys) => {
-      return (
-        <div key={keys} className="logo-column">
-          {console.log(logo)}
-          <img src={logo} alt="affiliate_logo" />
-        </div>
-      )
-    })}
-  </div>
-)
+const Affiliates = ({ logosArray }) => {
+  return (
+    <div className="logo-row">
+      {logosArray.map(({ logo }, keys) => {
+        return (
+          <div key={keys} className="logo-column">
+            {console.log(logo)}
+            <img src={logo} alt="affiliate_logo" />
+          </div>
+        )
+      })}
+    </div>    
+  )
+}
+
+
 
 Affiliates.propTypes ={
   images: PropTypes.shape({
