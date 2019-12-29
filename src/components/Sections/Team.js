ea82d7baecaafb68267808be3d9b2121eebb5143
amitp <amitpatel_7@msn.com>
2019-12-29 00:06:23 -0800

    added background image with overlay to Team section

--- src/components/Sections/Team.js
+++ src/components/Sections/Team.js
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types'
 
 const Team = ({ gridItems }) => {
   return (
-    <div className="team_section" style={
-      {
-        backgroundImage: 'url(/img/Avos.jpg)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center center'
-      }
-    }>
+    <div className="team_section">
       <h1 className="title" style={{color: 'black'}}>Meet Our Team</h1>
       {gridItems.map(({ member = {name: '', icon: '', title: '', description: ''} }, keys) => {
         return (
