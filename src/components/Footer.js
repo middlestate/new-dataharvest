3bb522fcb3680143f200ad73c6c4377cfeee51e4
amitp <amitpatel_7@msn.com>
2019-12-30 00:48:32 -0800

    added font awesome icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,18 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer>
-        <a href="https://mdst.typeform.com/to/SENQ20">
+        <a href="https://mdst.typeform.com/to/SENQ20" className='button_link'>
           <button>Get In Touch</button>        
         </a>
         <div className="links_container">
-          <a href="#">
-            <FontAwesomeIcon icon={['fab', 'facebook-square']} size="lg" />
+          <a href="#" className='link_icon'>
+            <FontAwesomeIcon icon={['fab', 'facebook-square']} size="3x" />
           </a>
-          <a href="#">
-            <FontAwesomeIcon icon={['fab', 'linkedin']} size="lg" />
+          <a href="#" className='link_icon'>
+            <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x" />
           </a>
-          <a href="#">
-            <img src="/img/MN.jpg" alt="MN" />
+          <a href="#" className='link_icon'>
+            <img src="/img/MN.jpg" alt="MN" style={{height: '30px'}} />
           </a>
         </div>
       </footer>
